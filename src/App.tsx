ef7d1c1be121959aba19422a969d27c84d3b1e53
MaximDantes <mfetisov2002@gmail.com>
2024-03-21 12:15:10 +0300

    feat: finish button layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,147 @@
 import { FC } from 'react'
-import Button from '@/components/Button/Button'
-import Svg1 from '@/assets/grade_FILL0_wght400_GRAD0_opsz24.svg'
+import MyButton from '@/components/Button/Button'
+import { Button } from '@mui/material'
+import Svg1 from '@/assets/arrow_circle_right_FILL0_wght400_GRAD0_opsz24.svg'
+import Svg2 from '@/assets/grade_FILL0_wght400_GRAD0_opsz24.svg'
 
 const App: FC = () => {
     return (
         <div>
-            <Button variant={'contained'} startIcon={<Svg1 />}>
-                button
-            </Button>
+            <div>
+                <MyButton startIcon={<Svg1 />} variant={'text'} size={'small'}>
+                    button
+                </MyButton>
+                <MyButton endIcon={<Svg2 />} variant={'text'} size={'medium'}>
+                    button
+                </MyButton>
+                <MyButton startIcon={<Svg1 />} endIcon={<Svg2 />} variant={'text'} size={'large'}>
+                    button
+                </MyButton>
+                <MyButton startIcon={<Svg1 />} variant={'outlined'} size={'small'}>
+                    button
+                </MyButton>
+                <MyButton endIcon={<Svg2 />} variant={'outlined'} size={'medium'}>
+                    button
+                </MyButton>
+                <MyButton startIcon={<Svg1 />} endIcon={<Svg2 />} variant={'outlined'} size={'large'}>
+                    button
+                </MyButton>
+                <MyButton startIcon={<Svg1 />} variant={'contained'} size={'small'} disabled>
+                    button
+                </MyButton>
+                <MyButton endIcon={<Svg2 />} variant={'text'} size={'medium'} disabled>
+                    button
+                </MyButton>
+                <MyButton startIcon={<Svg1 />} endIcon={<Svg2 />} variant={'outlined'} size={'large'} disabled>
+                    button
+                </MyButton>
+            </div>
+            <div>
+                <Button startIcon={<Svg1 />} variant={'text'} size={'small'}>
+                    button
+                </Button>
+                <Button endIcon={<Svg2 />} variant={'text'} size={'medium'}>
+                    button
+                </Button>
+                <Button startIcon={<Svg1 />} endIcon={<Svg2 />} variant={'text'} size={'large'}>
+                    button
+                </Button>
+                <Button startIcon={<Svg1 />} variant={'outlined'} size={'small'}>
+                    button
+                </Button>
+                <Button endIcon={<Svg2 />} variant={'outlined'} size={'medium'}>
+                    button
+                </Button>
+                <Button startIcon={<Svg1 />} endIcon={<Svg2 />} variant={'outlined'} size={'large'}>
+                    button
+                </Button>
+                <Button startIcon={<Svg1 />} variant={'contained'} size={'small'} disabled>
+                    button
+                </Button>
+                <Button endIcon={<Svg2 />} variant={'text'} size={'medium'} disabled>
+                    button
+                </Button>
+                <Button startIcon={<Svg1 />} endIcon={<Svg2 />} variant={'outlined'} size={'large'} disabled>
+                    button
+                </Button>
+            </div>
+
+            <div>
+                <MyButton variant={'contained'} size={'small'}>
+                    button
+                </MyButton>
+                <MyButton variant={'contained'} size={'medium'}>
+                    button
+                </MyButton>
+                <MyButton variant={'contained'} size={'large'}>
+                    button
+                </MyButton>
+                <MyButton variant={'text'} size={'small'}>
+                    button
+                </MyButton>
+                <MyButton variant={'text'} size={'medium'}>
+                    button
+                </MyButton>
+                <MyButton variant={'text'} size={'large'}>
+                    button
+                </MyButton>
+                <MyButton variant={'outlined'} size={'small'}>
+                    button
+                </MyButton>
+                <MyButton variant={'outlined'} size={'medium'}>
+                    button
+                </MyButton>
+                <MyButton variant={'outlined'} size={'large'}>
+                    button
+                </MyButton>
+                <MyButton variant={'contained'} size={'small'} disabled>
+                    button
+                </MyButton>
+                <MyButton variant={'text'} size={'medium'} disabled>
+                    button
+                </MyButton>
+                <MyButton variant={'outlined'} size={'large'} disabled>
+                    button
+                </MyButton>
+            </div>
+            <div>
+                <Button variant={'contained'} size={'small'}>
+                    button
+                </Button>
+                <Button variant={'contained'} size={'medium'}>
+                    button
+                </Button>
+                <Button variant={'contained'} size={'large'}>
+                    button
+                </Button>
+                <Button variant={'text'} size={'small'}>
+                    button
+                </Button>
+                <Button variant={'text'} size={'medium'}>
+                    button
+                </Button>
+                <Button variant={'text'} size={'large'}>
+                    button
+                </Button>
+                <Button variant={'outlined'} size={'small'}>
+                    button
+                </Button>
+                <Button variant={'outlined'} size={'medium'}>
+                    button
+                </Button>
+                <Button variant={'outlined'} size={'large'}>
+                    button
+                </Button>
+                <Button variant={'contained'} size={'small'} disabled>
+                    button
+                </Button>
+                <Button variant={'text'} size={'medium'} disabled>
+                    button
+                </Button>
+                <Button variant={'outlined'} size={'large'} disabled>
+                    button
+                </Button>
+            </div>
         </div>
     )
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,26 +1,47 @@
-import React, { ButtonHTMLAttributes, FC, ReactNode } from 'react'
+import React, { DOMAttributes, FC, ReactNode } from 'react'
 import styles from './Button.module.scss'
 
-type Props = {
-    variant?: 'text' | 'contained' | 'outlined'
-    startIcon?: ReactNode
-    endIcon?: ReactNode
-} & ButtonHTMLAttributes<HTMLButtonElement>
+type Props = Partial<{
+    variant: 'text' | 'contained' | 'outlined'
+    size: 'small' | 'medium' | 'large'
+    disabled: boolean
+    startIcon: ReactNode
+    endIcon: ReactNode
+    children: ReactNode
+    tabIndex: number
+    type: 'button' | 'submit' | 'reset'
+}> &
+    DOMAttributes<HTMLButtonElement>
 
-const Button: FC<Props> = (props) => {
+const Button: FC<Props> = ({ variant, size, disabled, startIcon, endIcon, tabIndex, children, type, ...props }) => {
     const classNames = [styles.button]
 
-    if (props.variant) {
-        classNames.push(styles[props.variant])
+    if (variant) {
+        classNames.push(styles[variant])
     } else {
         classNames.push(styles.contained)
     }
 
+    if (size) {
+        classNames.push(styles[size])
+    } else {
+        classNames.push(styles.medium)
+    }
+
+    if (disabled) classNames.push(styles.disabled)
+
     return (
-        <button className={classNames.join(' ')} {...props}>
-            {props.startIcon}
-            {props.children}
-            {props.endIcon}
+        <button
+            {...props}
+            className={classNames.join(' ')}
+            tabIndex={tabIndex ?? 0}
+            type={type ?? 'button'}
+            role={'button'}
+            disabled={disabled}
+        >
+            {startIcon && <span className={styles.icon}>{startIcon}</span>}
+            <span>{children}</span>
+            {endIcon && <span className={styles.icon}>{endIcon}</span>}
         </button>
     )
 }
