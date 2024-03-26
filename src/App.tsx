b07b702bf37585ea7f13f703c659d1d1865f455b
MaximDantes <mfetisov2002@gmail.com>
2024-03-26 15:54:13 +0300

    feat: add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import { FC, useState } from 'react'
-import { Switch } from '@mui/material'
-import MySwitch from '@/components/Switch/Switch'
+import Button from '@/components/Button/Button'
+import MyModal from '@/components/Modal/Modal'
 
 const App: FC = () => {
-    const [checked1, setChecked1] = useState(true)
-    const [checked2, setChecked2] = useState(false)
-
+    const [open, setOpen] = useState(false)
     return (
         <>
             <div>
-                <MySwitch checked={checked2} onChange={(e, checked) => setChecked2(checked)} />
-                <MySwitch checked={checked1} onChange={(e, checked) => setChecked1(checked)} />
-                <MySwitch checked={checked2} onChange={(e, checked) => setChecked2(checked)} disabled />
-                <MySwitch checked={checked1} onChange={(e, checked) => setChecked1(checked)} disabled />
-            </div>
-            <div>
-                <Switch checked={checked2} onChange={(e, checked) => setChecked2(checked)} />
-                <Switch checked={checked1} onChange={(e, checked) => setChecked1(checked)} />
-                <Switch checked={checked2} onChange={(e, checked) => setChecked2(checked)} disabled />
-                <Switch checked={checked1} onChange={(e, checked) => setChecked1(checked)} disabled />
+                <Button onClick={() => setOpen(true)}>1</Button>
+                <Button onClick={() => setOpen(true)}>2</Button>
+                <Button onClick={() => setOpen(true)}>3</Button>
+                <Button onClick={() => setOpen(true)}>4</Button>
+
+                <MyModal open={open} onClose={() => setOpen(false)}>
+                    <div>
+                        <Button onClick={() => setOpen(true)}>1</Button>
+                        <Button onClick={() => setOpen(true)}>2</Button>
+                        <Button onClick={() => setOpen(true)}>3</Button>
+                        <Button onClick={() => setOpen(true)}>4</Button>
+                    </div>
+                </MyModal>
             </div>
         </>
     )
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,89 @@
+import React, { FC, ReactElement, useEffect, useRef, KeyboardEvent, MouseEvent } from 'react'
+import { createPortal } from 'react-dom'
+import styles from './Modal.module.scss'
+
+type Props = {
+    open: boolean
+    onClose: () => void
+    children?: ReactElement | ReactElement[]
+}
+
+const Modal: FC<Props> = (props) => {
+    const trigger = useRef<HTMLButtonElement>()
+    const initialFocus = useRef<HTMLDivElement>()
+    const container = useRef<HTMLDivElement>()
+    const closeButton = useRef<HTMLButtonElement>()
+
+    useEffect(() => {
+        if (props.open) {
+            trigger.current = document.activeElement as HTMLButtonElement
+            initialFocus.current.focus()
+            document.body.style.overflow = 'hidden'
+        }
+
+        return () => {
+            document.body.style.removeProperty('overflow')
+            trigger.current?.focus()
+            trigger.current = null
+        }
+    }, [props.open])
+    if (!props.open) return
+
+    const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'Escape') {
+            props.onClose()
+        }
+    }
+
+    const handleClick = (e: MouseEvent<HTMLInputElement>) => {
+        if (e.target === e.currentTarget) {
+            props.onClose()
+        }
+    }
+
+    const moveFocusToStart = (e: KeyboardEvent<HTMLButtonElement>) => {
+        if (e.ctrlKey || e.altKey || e.shiftKey) return
+
+        if (e.key === 'Tab') {
+            initialFocus.current.focus()
+        }
+    }
+
+    const moveFocusToEnd = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (document.activeElement !== initialFocus.current) {
+            const firstFocusable = container.current.querySelectorAll(
+                'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+            )[0]
+
+            if (document.activeElement !== firstFocusable) return
+        }
+
+        if (e.ctrlKey || e.altKey) return
+
+        if (e.key === 'Tab' && e.shiftKey) {
+            closeButton.current.focus()
+        }
+    }
+
+    const modal = (
+        <div role={'dialog'} aria-modal className={styles.modal} onKeyDown={handleKeyDown}>
+            <div ref={initialFocus} tabIndex={-1} aria-hidden onKeyDown={moveFocusToEnd} />
+
+            <div ref={container} className={styles.container} onKeyDown={moveFocusToEnd} onClick={handleClick}>
+                {props.children}
+            </div>
+
+            <button
+                ref={closeButton}
+                aria-label={'close'}
+                className={styles['close-button']}
+                onClick={props.onClose}
+                onKeyDown={moveFocusToStart}
+            />
+        </div>
+    )
+
+    return createPortal(modal, document.body)
+}
+
+export default Modal
